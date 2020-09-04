f20750c9aee6e828c6552b0984a77c45b44ed151
Persiyan93 <persiyan.georgiev@gmail.com>
2020-09-04 09:47:16 +0300

    Bubble Sorting

---
+++ Sorting Algorithms/test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Sort(array);
+        }
+        static void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+
+                bool swap = false;
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swap = true;
+                    }
+                }
+                if (swap == false)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(string.Join(", ", array));
+        }
+       
+    }
+}
