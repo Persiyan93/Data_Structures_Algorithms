49bfca59954f87c04bf92881fb785c4adb3fc976
Persiyan93 <persiyan.georgiev@gmail.com>
2020-09-05 22:46:02 +0300

    Selection Sort

---
+++ Sorting Algorithms/Selection sort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Selection_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Sort(array);
+            Console.WriteLine(string.Join(", ", array));
+
+        }
+        public static void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+
+                    }
+
+                    if (minIndex == i)
+                    {
+                        break;
+                    }
+                    int temp = array[i];
+                    array[i] = array[minIndex];
+                    array[minIndex] = temp;
+
+                }
+            }
+
+        }
+    }
