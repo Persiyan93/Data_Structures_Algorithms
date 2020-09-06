748512dacb7c759b9f10a378a495b009bf68c11c
Persiyan93 <persiyan.georgiev@gmail.com>
2020-09-06 23:09:43 +0300

    Insertion sort

---
+++ Sorting Algorithms/Insertion Sort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Sort(array);
+            Console.WriteLine(string.Join(", ", array));
+        }
+        static void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int currentnumber = array[i];
+                int j = i - 1;
+
+                while (j>=0&&array[j]>currentnumber)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = currentnumber;
+            }
+        }
+    }
+}
