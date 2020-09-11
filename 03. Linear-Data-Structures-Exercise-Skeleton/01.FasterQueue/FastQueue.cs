c442844c0f2fd230f98efaa200e8477221266968
Persiyan93 <persiyan.georgiev@gmail.com>
2020-09-11 18:06:07 +0300

    Exercise linear data structure

---
+++ 03. Linear-Data-Structures-Exercise-Skeleton/01.FasterQueue/FastQueue.cs
@@ -0,0 +1,90 @@
+﻿namespace Problem01.FasterQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+
+    public class FastQueue<T> : IAbstractQueue<T>
+    {
+        private Node<T> _head;
+        private Node<T> lastElement;
+
+        public int Count { get; private set; }
+
+        public bool Contains(T item)
+        {
+            var current = this._head;
+
+            while (current != null)
+            {
+                if (current.Item.Equals(item))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public T Dequeue()
+        {
+            this.EnsureNotEmpty();
+
+            var headItem = this._head.Item;
+            var newHead = this._head.Next;
+            this._head.Next = null;
+            this._head = newHead;
+
+            this.Count--;
+
+            return headItem;
+        }
+
+        public void Enqueue(T item)
+        {
+            var node = new Node<T>(item);
+            if (_head==null)
+            {
+                _head = node;
+                lastElement = node;
+            }
+            else
+            {
+                lastElement.Next = node;
+                lastElement = node;
+
+            }
+            this.Count++;
+        }
+
+        public T Peek()
+        {
+            this.EnsureNotEmpty();
+
+            return this._head.Item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var currentNode = _head;
+            while (currentNode!=null)
+            {
+                yield return currentNode.Item;
+                currentNode = currentNode.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        private void EnsureNotEmpty()
+        {
+            if (this.Count == 0)
+                throw new InvalidOperationException();
+        }
+    }
+}
---
+++ 03. Linear-Data-Structures-Exercise-Skeleton/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,76 @@
+﻿namespace Problem02.DoublyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class DoublyLinkedList<T> : IAbstractLinkedList<T>
+    {
+        private Node<T> head;
+        private Node<T> laselElement;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T item)
+        {
+            var node = new Node<T>(item);
+            if (head==null)
+            {
+                head = node;
+                laselElement = head;
+            }
+            else
+            {
+                node.Next = head;
+                head.Previous = node;
+                head = node;
+            }
+        }
+
+        public void AddLast(T item)
+        {
+            var node = new Node<T>(item);
+            if (head==null)
+            {
+                head = node;
+                laselElement = head;
+
+            }
+            else 
+            {
+                laselElement.Previous = laselElement;
+
+            }
+        }
+
+        public T GetFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RemoveFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RemoveLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
