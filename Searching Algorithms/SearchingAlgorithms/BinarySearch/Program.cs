7620eef8d9f53deb28c6f86222b77ef9de3ebade
Persiyan93 <persiyan.georgiev@gmail.com>
2021-05-25 19:12:59 +0300

    Searching Algorithms

---
+++ Searching Algorithms/SearchingAlgorithms/BinarySearch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Please enter array :");
+            int[] arr = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Console.WriteLine("Position of searched number:");
+            Console.WriteLine(RecursiveBinarySearch(arr, 9, 0, 7));
+        }
+        private static int RecursiveBinarySearch(int[] arr, int searchedElement,int firstIndex,int lastIndex)
+        {
+            if (lastIndex >= firstIndex)
+            {
+
+                int middle =firstIndex+(lastIndex-firstIndex)/ 2;
+                if (arr[middle] == searchedElement)
+                {
+                    return middle;
+                }
+
+                else if (arr[middle] > searchedElement)
+                {
+                    RecursiveBinarySearch(arr, searchedElement, firstIndex, middle - 1);
+                }
+                else if (arr[middle] < searchedElement)
+                {
+                    RecursiveBinarySearch(arr, searchedElement, middle + 1, lastIndex);
+                }
+                
+            }
+            //Return -1 if number is not exist in array
+            return  -1;
+            
+        }
+    }
+}
